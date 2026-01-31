019280d00994ebb8e7789139f246883a1932fb07
Mario Cesario <mario.henrique.cesario@gmail.com>
2026-01-31 17:00:15 -0300

    Adicionando libs e estrutura para o GitHub Pages

---
+++ html/resources/list.js
@@ -0,0 +1,10 @@
+/* Minimal list.js defining the `list` object used by index.html */
+var list = {
+  'en': {
+    'Examples': {
+      'Demos': {
+        'Basic Scene (T1)': 'T1/index'
+      }
+    }
+  }
+};
